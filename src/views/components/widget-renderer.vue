b976bfc167f4b53b4baa2d4af2ad74d1fc093a35
jackyu <yujinjin9@126.com>
2026-09-15 17:12:22 +0800

    refactor(widget-renderer): 统一组件属性传递逻辑
    移除重复的getWidgetComponentAttributes调用，改用统一计算属性renderComponentAttributes传递组件属性，简化代码同时避免类型兼容问题

--- src/views/components/widget-renderer.vue
+++ src/views/components/widget-renderer.vue
@@ -2,10 +2,10 @@
     <!-- eslint-disable-next-line vue/no-v-html -->
     <div v-if="widgetData.code === widgetList.WIDGET_HTML.code" class="html-contents" v-html="htmlDefaultValue"></div>
     <div v-else-if="widgetData.code === widgetList.WIDGET_ALERT.code" class="alert-box">
-        <el-alert v-bind="getWidgetComponentAttributes(widgetData, renderValue)" />
+        <el-alert v-bind="renderComponentAttributes" />
     </div>
     <div v-else-if="widgetData.code === widgetList.WIDGET_DIVIDER.code" class="divider-box">
-        <el-divider v-bind="getWidgetComponentAttributes(widgetData, renderValue)">
+        <el-divider v-bind="renderComponentAttributes">
             {{ widgetData.defaultValue }}
         </el-divider>
     </div>
@@ -29,80 +29,80 @@
         <el-input
             v-if="widgetData.code === widgetList.WIDGET_TEXT.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-input-number
             v-else-if="widgetData.code === widgetList.WIDGET_INPUT_NUMBER.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-radio-group
             v-else-if="widgetData.code === widgetList.WIDGET_RADIO_GROUP.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-checkbox-group
             v-else-if="widgetData.code === widgetList.WIDGET_CHECKBOX_GROUP.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-select
             v-else-if="widgetData.code === widgetList.WIDGET_SELECT.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-cascader
             v-else-if="widgetData.code === widgetList.WIDGET_CASCADER.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-date-picker
             v-else-if="widgetData.code === widgetList.WIDGET_DATE_PICKER.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
-        <el-time-picker v-else-if="widgetData.code === widgetList.WIDGET_TIME_PICKER.code" v-model="renderValue" v-bind="getWidgetComponentAttributes(widgetData, renderValue)" />
+        <el-time-picker v-else-if="widgetData.code === widgetList.WIDGET_TIME_PICKER.code" v-model="renderValue" v-bind="renderComponentAttributes" />
         <el-time-select
             v-else-if="widgetData.code === widgetList.WIDGET_TIME_SELECT.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-switch
             v-else-if="widgetData.code === widgetList.WIDGET_SWITCH.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-rate
             v-else-if="widgetData.code === widgetList.WIDGET_RATE.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-color-picker
             v-else-if="widgetData.code === widgetList.WIDGET_COLOR_PICKER.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-slider
             v-else-if="widgetData.code === widgetList.WIDGET_SLIDER.code"
             v-model="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         />
         <el-upload
             v-else-if="widgetData.code === widgetList.WIDGET_UPLOAD.code"
             v-model:file-list="renderValue"
-            v-bind="getWidgetComponentAttributes(widgetData, renderValue)"
+            v-bind="renderComponentAttributes"
             v-on="getWidgetComponentEvents(widgetData, formData, widgetFormData)"
         >
             <el-button type="primary">点击上传</el-button>
@@ -113,7 +113,7 @@
 import { computed, type PropType } from "vue";
 import { type WidgetFormData, type WidgetNormalData } from "@/views/composables/types";
 import { useFormItemRules } from "@/views/composables/widgets/form";
-import { getWidgetComponentAttributes, getWidgetComponentEvents, getWidgetList } from "@/views/composables/widget-registry";
+import { getWidgetComponentEvents, getWidgetList } from "@/views/composables/widget-registry";
 
 const props = defineProps({
     widgetData: {
@@ -133,10 +133,20 @@ const props = defineProps({
 const renderValue = defineModel<any>({
     required: true
 });
+
+/**
+ * @description 当前 Widget 已物化的组件运行属性。
+ * @remarks 统一为渲染层的宽属性视图，避免 Vue TS Plugin 将 Widget 联合类型中的不同 Props 直接传给单个 Element Plus 组件；这里仅保留原缓存引用，不负责构建或写回属性。
+ */
+const renderComponentAttributes = computed<Record<string, any>>(() => props.widgetData.componentAttributes ?? {});
+
+// HTML 组件默认值
 const htmlDefaultValue = computed(() => {
     const settingData = (props.widgetData.settingData || {}) as Record<string, unknown>;
     return typeof settingData.defaultValue === "string" ? settingData.defaultValue : "";
 });
+
+// 表单组件验证规则配置
 const formItemRuleConfig = computed(() => {
     const settingData = (props.widgetData.settingData || {}) as Record<string, unknown>;
     const componentFunctions = (props.widgetData.componentFunctions || {}) as Record<string, unknown>;
@@ -148,6 +158,8 @@ const formItemRuleConfig = computed(() => {
         validate: typeof componentFunctions.validate === "string" ? componentFunctions.validate : null
     };
 });
+
+// 组件列表
 const widgetList = getWidgetList();
 </script>
 <style lang="scss" scoped>
